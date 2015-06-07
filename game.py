1eadea9dd340648b9e06515fe57f1340edf56d9a
Deathwalkerz <deathwalker@mail.bg>
2015-06-08 01:18:11 +0300

    Basic implementation for the rats and maze

---
+++ game.py
@@ -0,0 +1,59 @@
+class Rat:
+    def __init__(self):
+        """
+        Initialize the rat.
+        """
+
+        pass
+
+    def set_location(self):
+        """
+        Set the rat's position.
+        """
+        pass
+
+    def eat_sprout(self):
+        """
+        Add to the rat's eaten sprouts.
+        """
+        pass
+
+    def __str__(self):
+        """
+        Return a string representation of the rat.
+        """
+        pass
+
+
+class Maze:
+    def __init__(self):
+        """
+        Initialize the maze.
+        """
+        pass
+
+    def is_wall(self):
+        """
+        Check if there is a wall in the given position.
+        """
+        pass
+
+    def get_character(self):
+        """
+        Return the character in the given position.
+        """
+        pass
+
+    def move(self):
+        """
+        Move the rat in the given direction.
+        Check for a wall.
+        Check for a sprout.
+        """
+        pass
+
+    def __str__(self):
+        """
+        Return a string representation of the maze.
+        """
+        pass
